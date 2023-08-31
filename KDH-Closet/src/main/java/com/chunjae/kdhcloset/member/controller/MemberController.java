bf32de48081afbb890aae6bdf2a436a1c6264cf8
starMinK <rbals040329@gmail.com>
2023-08-31 21:42:16 +0900

    commit

--- KDH-Closet/src/main/java/com/chunjae/kdhcloset/member/controller/MemberController.java
+++ KDH-Closet/src/main/java/com/chunjae/kdhcloset/member/controller/MemberController.java
@@ -8,6 +8,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
